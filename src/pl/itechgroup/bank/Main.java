243e5be768c0cb775f91e5ff9be4dca66d7cdb29
ZSE <piotrszkolazseii@gmail.com>
2021-10-29 08:57:16 +0200

    7.4

--- src/pl/itechgroup/bank/Main.java
+++ src/pl/itechgroup/bank/Main.java
@@ -1,28 +1,31 @@
 package pl.itechgroup.bank;
 
+import pl.itechgroup.bank.account.Account;
+import pl.itechgroup.bank.account.Person;
+import pl.itechgroup.bank.account.Transfer;
+import pl.itechgroup.bank.account.TransferService;
+import pl.itechgroup.bank.exceptions.AccountNotFoundExeption;
+
 public class Main {
     public static void main(String[] args) {
-        String string = "nfoanmxroce";
-        char chars[] = new char[8];
 
-        for (int i = 0, a = 0; i < string.length(); i++) {
+        Person p1 =  new Person("Piotr","B","23485963",19, 176);
+        p1.setAccount(new Account("Person blog", "66f5sdf58s", "plogb@pocz.com", 55541, 55.6f));
+        Person p2 = new Person("Mateusz", "K", "56659224", 22, 177);
+        //p2.setAccount(new Account("mammaK", "+5edf.#$%", "matkom@gmail.com", 54166, 115.9f));
+
+        Transfer transfer;
+        while (true)
+        {
             try {
-                chars[i] = string.charAt(i);
-            }
-            catch (Exception e)
-            {
+                transfer = TransferService.transfer(p1, p2, 32.4f);
                 break;
+            } catch (AccountNotFoundExeption e) {
+                p1.setAccount(new Account("","","",0,0f));
+                p2.setAccount(new Account("","","",0,0f));
             }
-            finally {
-                System.out.println(i);
-            }
-
-
-
         }
 
-
-
-        System.out.println(chars);
+        System.out.println(transfer);
     }
 }
--- src/pl/itechgroup/bank/account/TransferService.java
+++ src/pl/itechgroup/bank/account/TransferService.java
@@ -5,20 +5,16 @@ import pl.itechgroup.bank.exceptions.AccountNotFoundExeption;
 import java.util.Date;
 public class TransferService {
 
-    public static Transfer transfer(Person sender, Person receiver, float ammout) throws AccountNotFoundExeption {
-        if (checkAccountBalance(sender, ammout)) {
-            if (sender.getAccount()==null || receiver.getAccount()==null) throw new AccountNotFoundExeption();
-            sender.getAccount().money -= ammout;
-            receiver.getAccount().money += ammout;
-            return new Transfer(sender, receiver, ammout, new Date());
+    public static Transfer transfer(Person sender, Person receiver, float amount) throws AccountNotFoundExeption {
+        if (sender.getAccount()==null || receiver.getAccount()==null) throw new AccountNotFoundExeption();
+        if (checkAccountBalance(sender, amount)) {
+            sender.getAccount().money -= amount;
+            receiver.getAccount().money += amount;
         }
-        return null;
+        return new Transfer(sender, receiver, amount, new Date());
     }
 
-    private static boolean checkAccountBalance(Person sender, float ammout) {
-        if(sender.getAccount().money >= ammout) {
-            return true;
-        }
-        return false;
+    private static boolean checkAccountBalance(Person sender, float amount) {
+        return (sender.getAccount().money >= amount);
     }
 }
